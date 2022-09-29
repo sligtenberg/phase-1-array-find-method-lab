c3c355a123481794c4a2e2c8b6a40712398105b7
stephen ligtenberg <bountyhunter24@gmail.com>
2022-09-28 23:46:26 -0400

    commit tree ba3486f41fe7f7d2cf33f0533c7fe38dabd354bc

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record) {
+    if (record.find(isAWin)) {
+        return record.find(isAWin).year;
+    }
+}
+
+const isAWin = (element) => element.result === "W";
